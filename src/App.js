e8ddb929935d63de94f6f91157b2201450e90341
Julia-swift <strizhj@gmail.com>
2022-12-25 21:03:54 +0200

    Update App.js
    Github URL added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <p>
+        {" "}
+        <footer>
+          <a
+            href="https://github.com/Julia-swift/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          {""} by Yuliya Stryzhova
+        </footer>
+      </p>
     </div>
   );
 }
